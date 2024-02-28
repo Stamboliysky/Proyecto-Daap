61c0d9613b9d0ac37299b6d4ce2e347de872694e
Stamboliysky <jordanspassoff@gmail.com>
2024-02-28 13:32:58 +0100

    Añadir Header en Daap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 function App() {
-  return<h1 className="text-3xl font-bold underline bg-red-200">Hello world!</h1>
+  return<>
+  </><h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 export default App
