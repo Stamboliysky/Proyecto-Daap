520f6e436cff9a942f3d9ff8af6c4ef08832b649
Stamboliysky <jordanspassoff@gmail.com>
2024-02-28 14:11:06 +0100

    añadir coreccion App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+
+import Header from "./componens/Header"
 function App() {
-  return<>
-  </><h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return
+  <>
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 export default App
