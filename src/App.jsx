8710ab9db7defe9843c816bd2e244a46fa7ad656
Stamboliysky <jordanspassoff@gmail.com>
2024-02-28 09:56:43 +0100

    Añadir Ejercicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return<h1 className="text-3xl font-bold underline bg-red-200">Hello world!</h1>
+  return<><h1 className="text-3xl font-bold underline bg-red-200">Hello world!</h1><div>Soy el Padre</div><div> Soy el <Hijo></Hijo></div></>
 }
 
 export default App
