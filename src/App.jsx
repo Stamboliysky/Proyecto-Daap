665122059afb2728823b461c3e4e019b7fb5343d
Stamboliysky <jordanspassoff@gmail.com>
2024-02-29 06:34:28 +0100

    Eliminar Heder y Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 
-import Header from "./componens/Header"
+
+
+
 function App() {
-  return
-  <>
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+  
+      
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    
+)
 }
 export default App
